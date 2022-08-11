34d2b6b58a0540f779ca190c5d3b38429feb6537
ImJars <ra374885@uaeh.edu.mx>
2022-08-10 20:02:52 -0500

    HoverEffect Pending - LinkHeader

--- src/components/LinkHeader.jsx
+++ src/components/LinkHeader.jsx
@@ -2,15 +2,22 @@ import React from 'react'
 import styled from 'styled-components';
 
 const LinkHeaderStyled = styled.a`
-    color: black;
-	background-image: linear-gradient(to bottom, transparent 65%, #3963ec 0);
-	background-size: 0% 100%;
-	background-repeat: no-repeat;
-	transition: background-size .4s ease;
+    background: linear-gradient(to bottom, #3549ff, #3549ff) 50% calc(100% - 2px) / 25px 2px no-repeat;
+    color: #111;
+    font: normal 600 20px / 1.35 "Work Sans", Helvetica, Arial, Verdana, sans-serif;
+		margin: -1px;
+    margin-bottom: -2px !important;
+    padding: 1px 5px 5px 3px;
+    text-decoration: none !important;
 
-    &:hover {
-        background-size: 100% 100%;
-	    cursor: pointer;
+    -webkit-transition: all .35s ease-in-out;
+            transition: all .35s ease-in-out;
+
+    &:hover, :focus {
+        background-size: 100% 2px;
+
+    -webkit-transition: all .35s ease-in-out !important;
+            transition: all .35s ease-in-out !important;
     }
 `
 
