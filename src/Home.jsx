ce7b460532b034edf77dc6d63d0286b64a5add93
ImJars <ra374885@uaeh.edu.mx>
2022-08-19 22:22:55 -0500

    safe changes

--- src/Home.jsx
+++ src/Home.jsx
@@ -33,11 +33,6 @@ const Home = () => {
                                 creating <font className='text-Color-Text-H3'>Web3</font> technology products.
                             </h3>
                             {/* <RotatingText/> */}
-                            {/* <button className="mt-4 relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200">
-                                <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-Background-white rounded-md group-hover:bg-opacity-0">
-                                    Descargar CV
-                                </span>
-                            </button> */}
                         </div>
                         <div className='flex justify-center'>
                             <img className='h-96' src={Img} alt="" />
