70ef3a9d75535786ce72e01ca358032998c92752
ImJars <ra374885@uaeh.edu.mx>
2022-09-13 22:52:56 -0500

    Add InformationObserver-style

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import Menu from './Menu';
 import { useInView } from 'react-intersection-observer';
 import { useAnimation  } from 'framer-motion';
 import Home from './Home';
+import Information from './information';
 import About from './about';
 import Work from './Work';
 
 const App = () => {
     const { ref, inView } = useInView({
-        threshold: 0.2,
+        threshold: 0.1,
     });
     
     const animation = useAnimation();
+    const animationInformation = useAnimation();
+
 
     useEffect(() => {
         if (inView) {
@@ -22,6 +25,12 @@ const App = () => {
                     type: 'spring', duration: 1, bounce: 0.3
                 },
             });
+            animationInformation.start({
+                color: '#011326',
+                transition: {
+                    type: 'spring', duration: .1, bounce: 0.1
+                },
+            });
         }
         if (!inView) {
             animation.start({
@@ -29,13 +38,21 @@ const App = () => {
                 boxShadow: '0px 0px 0px 0px #011326',
                 transition: {
                     type: 'spring', duration: 1, bounce: 0.3
+                }, 
+            });
+            animationInformation.start({
+                position: 'fixed',
+                color: '#A4A4A4',
+                transition: {
+                    type: 'spring', duration: .1, bounce: 0.1
                 },
-                
             });
         }
     },[inView]);
+
     return ( 
         <div className='w-full'>
+            <Information animationInfo={animationInformation}/>
             <Menu animation={animation}/>
             <Home/>
             <div ref={ref}>
--- src/information.jsx
+++ src/information.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { SiTwitter, SiInstagram } from 'react-icons/si'
 import { FaCodepen, FaLinkedinIn } from 'react-icons/fa'
 import { FiGithub } from 'react-icons/fi'
+import { motion } from 'framer-motion';
+
 
 import styled from 'styled-components';
 
@@ -18,91 +20,93 @@ const Section = styled.div`
 }
 `
 
-const Information = () => {
+const Information = ({animationInfo }) => {
     return ( 
         <>
-            <Section
-                className="fixed text-Color-Text-H4 flex justify-end transform left-auto 
-                        rotate-90 -translate-y-3 translate-x-14 origin-left invisible sm:visible"
-            >
-                <div className='grid grid-cols-5 gap-x-6'>
-                    <a 
-                        href="https://twitter.com/Im_Jars"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <SiTwitter 
-                            className='h-5 w-5 -rotate-90 text-icon-color transition 
-                            ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
-                            duration-200'
-                        />
-                    </a>
-                    <a 
-                        href="https://www.instagram.com/jars.little/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <SiInstagram 
-                            className='h-5 w-5 -rotate-90 text-icon-color transition 
-                            ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
-                            duration-200'
-                        />
-                    </a>
-                    <a 
-                        href="https://github.com/ImJars"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FiGithub 
-                            className='h-5 w-5 -rotate-90 text-icon-color transition 
-                            ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
-                            duration-200'
-                        />
-                    </a>
-                    <a 
-                        href="https://www.linkedin.com/in/angel-ramirez-08468212b/"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaLinkedinIn 
-                            className='h-5 w-5 -rotate-90 text-icon-color transition 
-                            ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
-                            duration-200'
-                        />
-                    </a>
-                    <a 
-                        href="https://codepen.io/Im_Jars"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <FaCodepen 
-                            className='h-5 w-5 -rotate-90 text-icon-color transition 
-                            ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
-                            duration-200'
-                        />
-                    </a>
-                </div>
-                <div className='grid content-center ml-5' >
-                    <hr className='line'/>
-                </div>
-            </Section>
-            <Section
-                className="fixed justify-end flex rotate-90 -translate-y-1/2 translate-x-1/5
-                right-0 top-1/2 z-50 left-auto invisible sm:visible"
-            >
-                <div 
-                    className='font-roboto text-sm text-icon-color transition : ;
-                            ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
-                            duration-200 '
+            <motion.div animate={animationInfo}>
+                <Section
+                    className="fixed flex justify-end transform left-auto 
+                            rotate-90 -translate-y-3 translate-x-14 origin-left invisible sm:visible"
                 >
-                    <a href="mailto:hi.legacyier@gmail.com">
-                        hi.legacyier@gmail.com
-                    </a>
-                </div>
-                <div className='grid content-center ml-5' >
-                    <hr className='line'/>
-                </div>
-            </Section>
+                    <div className='grid grid-cols-5 gap-x-6'>
+                        <a 
+                            href="https://twitter.com/Im_Jars"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <SiTwitter 
+                                className='h-5 w-5 -rotate-90 transition 
+                                ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
+                                duration-200'
+                            />
+                        </a>
+                        <a 
+                            href="https://www.instagram.com/jars.little/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <SiInstagram 
+                                className='h-5 w-5 -rotate-90 transition 
+                                ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
+                                duration-200'
+                            />
+                        </a>
+                        <a 
+                            href="https://github.com/ImJars"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FiGithub 
+                                className='h-5 w-5 -rotate-90 transition 
+                                ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
+                                duration-200'
+                            />
+                        </a>
+                        <a 
+                            href="https://www.linkedin.com/in/angel-ramirez-08468212b/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaLinkedinIn 
+                                className='h-5 w-5 -rotate-90 transition 
+                                ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
+                                duration-200'
+                            />
+                        </a>
+                        <a 
+                            href="https://codepen.io/Im_Jars"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            <FaCodepen 
+                                className='h-5 w-5 -rotate-90 transition 
+                                ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
+                                duration-200'
+                            />
+                        </a>
+                    </div>
+                    <div className='grid content-center ml-5' >
+                        <hr className='line'/>
+                    </div>
+                </Section>
+                <Section
+                    className="fixed justify-end flex rotate-90 -translate-y-1/2 translate-x-1/5
+                    right-0 top-1/2 z-50 left-auto invisible sm:visible"
+                >
+                    <div 
+                        className='font-roboto text-sm transition
+                                ease-in-out hover:-translate-x-1 hover:text-Color-Text-H3 
+                                duration-200 '
+                    >
+                        <a href="mailto:hi.legacyier@gmail.com">
+                            hi.legacyier@gmail.com
+                        </a>
+                    </div>
+                    <div className='grid content-center ml-5' >
+                        <hr className='line'/>
+                    </div>
+                </Section>
+            </motion.div>
         </>
      );
 }
