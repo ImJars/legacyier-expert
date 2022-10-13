83c184b7939e920c683d4277ad0e11e114d3b20f
ImJars <ra374885@uaeh.edu.mx>
2022-10-12 22:11:32 -0500

    scrollMenu-initial

---
+++ src/scrollMenu.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ScrollMenu = () => {
+    return ( 
+        <>
+            <div>
+                
+            </div>
+        </>
+     );
+}
+ 
+export default ScrollMenu;
