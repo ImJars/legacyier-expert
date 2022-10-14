c460c5e75a2274a243f860ddd5adeffe0c341410
ImJars <ra374885@uaeh.edu.mx>
2022-10-13 19:58:57 -0500

    Add button-animation

--- src/scrollMenu.jsx
+++ src/scrollMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BsFillArrowUpCircleFill } from 'react-icons/bs'
+import { AiOutlineArrowUp } from 'react-icons/ai'
 
 const ScrollMenu = () => {
     return ( 
@@ -8,13 +8,15 @@ const ScrollMenu = () => {
                 className='fixed bottom-0 right-0 z-50'
             >
                 <div
-                    className='mb-10 mr-10'
+                    className='mb-2 mr-2 bg-Background rounded-full p-5 
+                            cursor-pointer '
                 >
                     <a 
                         href="#Home"
-                        className='text-xl text-white'
+                        className='text-xl text-white '
                     >
-                        <BsFillArrowUpCircleFill />
+                        <AiOutlineArrowUp 
+                        className='animate-pulse hover:scale-150'/>
                     </a>
                 </div>
             </div>
